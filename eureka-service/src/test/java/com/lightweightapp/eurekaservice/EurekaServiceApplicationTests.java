d27d99d3949feefa83f327e0a474bd93c1374bab
Ayez <ayezele.mboto@exquisappfactory.com>
2023-03-07 20:51:21 +0100

    Eureka service registry created.

---
+++ eureka-service/src/test/java/com/lightweightapp/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lightweightapp.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
