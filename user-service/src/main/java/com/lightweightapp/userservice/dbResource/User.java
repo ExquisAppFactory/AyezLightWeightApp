a696d4e788d1aacf41d70f95b83525104e483126
Ayez <ayezele.mboto@exquisappfactory.com>
2023-03-07 15:27:05 +0100

    User Service, user entity class, updated.

--- user-service/src/main/java/com/lightweightapp/userservice/dbResource/User.java
+++ user-service/src/main/java/com/lightweightapp/userservice/dbResource/User.java
@@ -1,4 +1,70 @@
 package com.lightweightapp.userservice.dbResource;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users", schema = "light_weight_app")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;
+
+    public User() {
+    }
+
+    public User(int id, String firstName, String lastName, String email, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
