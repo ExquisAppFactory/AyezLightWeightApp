606c8889f5ae33b5358b1ef0122e720aaed369e9
Ayez <ayezele.mboto@exquisappfactory.com>
2023-03-07 17:08:17 +0100

    User model class updated to include required fields, getter and setter methods, no argument constructor and a constructor with required fields passed as a constructor.

--- user-service/src/main/java/com/lightweightapp/userservice/model/UserModel.java
+++ user-service/src/main/java/com/lightweightapp/userservice/model/UserModel.java
@@ -1,4 +1,61 @@
 package com.lightweightapp.userservice.model;
 
 public class UserModel {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String status;
+
+
+    public UserModel() {
+    }
+
+    public UserModel(String firstName, String lastName, String email, String password, String status) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.status = status;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
