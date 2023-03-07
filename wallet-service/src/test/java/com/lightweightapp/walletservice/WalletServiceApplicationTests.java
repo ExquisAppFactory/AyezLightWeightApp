a3bb242ad3bf9536c4fa6cc19a7f874679be9768
Ayez <ayezele.mboto@exquisappfactory.com>
2023-03-07 15:02:00 +0100

    Wallet service created.

---
+++ wallet-service/src/test/java/com/lightweightapp/walletservice/WalletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lightweightapp.walletservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
