cbf0fd6f82adfe1eca1d8b82a0dfef069da3fc8d
Ayez <ayezele.mboto@exquisappfactory.com>
2023-03-07 17:25:40 +0100

    Method to fetch user information created and class to model the response payload created.

--- user-service/src/main/java/com/lightweightapp/userservice/controller/UserServiceResource.java
+++ user-service/src/main/java/com/lightweightapp/userservice/controller/UserServiceResource.java
@@ -1,4 +1,34 @@
 package com.lightweightapp.userservice.controller;
 
+import com.lightweightapp.userservice.dbResource.User;
+import com.lightweightapp.userservice.model.UserResponseModel;
+import com.lightweightapp.userservice.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 public class UserServiceResource {
+
+    private UserRepository userRepository;
+
+    public UserServiceResource(UserRepository userRepository)
+    {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/{userid}")
+    public UserResponseModel getUser(@PathVariable("userid") final int userid)
+    {
+        User user = userRepository.findById(userid);
+        UserResponseModel userResponseModel =  new UserResponseModel();
+        userResponseModel.setRequestStatus(HttpStatus.OK.getReasonPhrase());
+        userResponseModel.setResponseMessage("User record fetched successfully!");
+        UserResponseModel.Data data = userResponseModel.new Data();
+        data.setFirstName(user.getFirstName());
+        data.setLastName(user.getLastName());
+        data.setUserId(user.getId());
+        data.setEmail(user.getEmail());
+        userResponseModel.setData(data);
+        return userResponseModel;
+    }
 }
