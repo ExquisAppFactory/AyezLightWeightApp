28b9ae9091cd875aeb9c72269182a32f18de9f3a
Ayez <ayezele.mboto@exquisappfactory.com>
2023-03-15 23:24:13 +0100

    Created Authorization Service

--- user-service/src/main/java/com/lightweightapp/userservice/controller/UserAuthResource.java
+++ user-service/src/main/java/com/lightweightapp/userservice/controller/UserAuthResource.java
@@ -1,4 +1,11 @@
 package com.lightweightapp.userservice.controller;
 
+import com.lightweightapp.userservice.repository.UserRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user/register")
 public class UserAuthResource {
+
 }
