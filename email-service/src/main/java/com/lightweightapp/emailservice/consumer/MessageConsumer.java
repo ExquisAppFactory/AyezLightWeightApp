b7d0bb91223edb7d6fd0aa05e8bc36340dfedfdf
Ayez <ayezele.mboto@exquisappfactory.com>
2023-03-21 18:56:02 +0100

    Removed Authentication service. Implemented consumer pull for message broker on email service

--- email-service/src/main/java/com/lightweightapp/emailservice/consumer/MessageConsumer.java
+++ email-service/src/main/java/com/lightweightapp/emailservice/consumer/MessageConsumer.java
@@ -1,4 +1,16 @@
 package com.lightweightapp.emailservice.consumer;
 
+import com.lightweightapp.emailservice.configuration.MessageQueueConfig;
+import com.lightweightapp.emailservice.model.UserVerificationModel;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MessageConsumer {
+
+    @RabbitListener(queues = MessageQueueConfig.QUEUE)
+    public void consumerMessageFromQueue(UserVerificationModel userVerificationModel)
+    {
+        System.out.println("Hi");
+    }
 }
